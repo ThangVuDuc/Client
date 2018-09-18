a55bb4cf41b09e1bf7ca6c1d67e134af1f94f736
PhungMinhHoang <phungminhhoang174@gmail.com>
2018-09-18 10:28:08 +0700

    Update Profile.js

--- Profile/src/component/Profile.js
+++ Profile/src/component/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Container, Nav, Navbar, NavItem, NavLink, Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap"
 import ProfileSidebar from './ProfileSidebar'
 import ProfileContent from './ProfileContent';
+import ListShop from './ListShop';
+
 import OrderHistory from './OrderHistory'
 class Profile extends Component {
     render() {
@@ -12,7 +14,8 @@ class Profile extends Component {
                         <ProfileSidebar />
                     </Col>
                     <Col md='9'>
-                        <ProfileContent />
+                        <ListShop/>
+                        {/* <ProfileContent /> */}
                         {/* <OrderHistory/> */}
                     </Col>
                 </Row>
