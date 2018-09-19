32669d6d9ca32dadebb63371d951c70d1995f58c
Regular Guy <Buiduk@gmail.com>
2018-09-19 19:52:08 +0700

    OK
    OK

--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,4 @@
-export const ROOT_API = 'https://backend-foddyhola.herokuapp.com/api';
+// export const ROOT_API = 'https://backend-foddyhola.herokuapp.com/api';
 
-// export const ROOT_API = 'https://localhost:8080/api';
+export const ROOT_API = 'https://localhost:8080/api';
 
